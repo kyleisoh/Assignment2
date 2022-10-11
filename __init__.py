7dac56641b930b747222094f55781463ad2dbd3f
Kyle <inohkr@gmail.com>
2022-10-11 16:26:44 -0400

    Created Cart, Item classes

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+# Third Party Imports
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -9,30 +10,30 @@ api = Api(app)
 app.config['SECRET_KEY'] = 'csc301'
 
 
-class CartForm(FlaskForm):
+class ItemForm(FlaskForm):
 
-    cart = StringField("What item is in the cart?")
+    item = StringField("What item is in the cart?")
     submit = SubmitField('Ready to checkout?')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    cart = False
-    form = CartForm()
+    item = False
+    form = ItemForm()
 
     if form.validate_on_submit():
-        cart = form.cart.data
-        form.cart.date = ''
-    return render_template('index.html', form=form, cart=cart)
+        item = form.item.data
+        form.item.date = ''
+    return render_template('index.html', form=form, item=item)
 
 
-class Cart(Resource):
+class Item(Resource):
 
     def get(self):
         return {'apple': '1'}
 
 
-api.add_resource(Cart, '/')
+api.add_resource(Item, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ item.py
@@ -0,0 +1,7 @@
+class Item():
+
+    def __init__(self, id, name, price, quantity):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.quantity = quantity
