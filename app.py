cb6da706d42342db74b495e30eed52392735abc4
Kyle InSeok Oh <inohkr@gmail.com>
2022-10-14 11:41:48 -0400

    Removed flask forms

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import json
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
-from flask_wtf import FlaskForm
-from forms import ItemForm
 
 ####### Setup/Config #######
 app = Flask(__name__)
@@ -14,16 +12,11 @@ app.config['SECRET_KEY'] = SECRET_KEY
 api = Api(app)
 
 ####### Route Decorators #######
-
-
 @app.route('/', methods=['GET', 'POST'])
 @cross_origin()
-# Index Page
 def index():
     form = ItemForm()
     if (request.method == "POST"):
-        if form.validate_on_submit():
-            item_name = form.item_name.data
         jsonData = request.get_json()
         print(jsonData)
         return {
--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, DecimalField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class ItemForm(FlaskForm):
-    item_name = StringField("name", validators=[DataRequired()])
